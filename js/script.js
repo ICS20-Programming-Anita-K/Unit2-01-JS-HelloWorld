b268a9f8d22405e5ff3388b13cccbc1beefbad63
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-03-10 05:30:29 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Anita Kay All rights reserved
+//
+// Created by: Anita Kay
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = "How do celebrities stay cool? Because they have many fans!"
+}
